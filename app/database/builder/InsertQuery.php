8d0113c4c0d41f569cf1bed5954c6c4c991179fe
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2025-11-07 19:41:44 -0400

    Classe de Insert

--- app/database/builder/InsertQuery.php
+++ app/database/builder/InsertQuery.php
@@ -2,20 +2,39 @@
 
 namespace app\database\builder;
 
+use app\database\Connection;
+
 class InsertQuery
 {
     private string $table;
+    private array $FieldsAndValues = [];
+    #Obter o nome da tabela onde os dados serão inserido.
     public static function table(string $table): self
     {
         $self = new self;
         $self->table = $table;
         return $self;
     }
-    public function save(array $FieldsAndValues)
+    private function createQuery(): string
     {
-        $key = implode(',', array_keys($FieldsAndValues));
-        $placeHolder = ':' . implode(',:', array_keys($FieldsAndValues));
-        $query = "insert into $this->table ($key) values ($placeHolder); ";
-        echo $query;
+        $fields  = implode(',', array_keys($this->FieldsAndValues));
+        $placeHolder = ':' . implode(',:', array_keys($this->FieldsAndValues));
+        return "insert into $this->table ($fields) values ($placeHolder);";
+    }
+    private function execute(string $query): bool
+    {
+        $con = Connection::connection();
+        $prepare = $con->prepare($query);
+        return $prepare->execute($this->FieldsAndValues);
+    }
+    public function save(array $FieldsAndValues): bool
+    {
+        $this->FieldsAndValues = $FieldsAndValues;
+        $query = $this->createQuery();
+        try {
+            return $this->execute($query);
+        } catch (\PDOException $e) {
+            throw new \Exception($e->getMessage());
+        }
     }
 }
