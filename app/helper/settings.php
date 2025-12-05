62039f6ddd0426778cb6f50059152bd5eb3a29f9
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2025-12-04 20:41:32 -0400

    Commit

--- app/helper/settings.php
+++ app/helper/settings.php
@@ -9,11 +9,4 @@ define('DIR_VIEW', ROOT . '/app/view');
 #$_SERVER['HTTP_HOST'] : Indica o domínio (host) que foi chamado na URL pelo navegador. Domínio principal meusite.com ou localhost
 #$_SERVER['REQUEST_SCHEME'] : Indica o protocolo usado na requisição atual. podendo ser http ou https
 #Criamos uma constante chamada HOME que guarda automaticamente o endereço principal do site.
-define('HOME', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST']);
-
-
-echo "<pre>";
-var_dump($_SERVER);
-echo "<br>";
-var_dump(HOME);
-die;
+define('HOME', $_SERVER['HTTP_CF_VISITOR'] . '://' . $_SERVER['HTTP_HOST']);
