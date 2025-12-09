8f57b224f84f26b37a67f37ca4af12bd23e45878
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2025-12-08 21:12:33 -0400

    Commit

--- app/controller/Login.php
+++ app/controller/Login.php
@@ -2,149 +2,16 @@
 
 namespace app\controller;
 
-use app\database\builder\InsertQuery;
-use app\database\builder\SelectQuery;
-use app\database\builder\UpdateQuery;
-
 class Login extends Base
 {
     public function login($request, $response)
     {
-        try {
-            $dadosTemplate = [
-                'titulo' => 'Autenticação'
-            ];
-            return $this->getTwig()
-                ->render($response, $this->setView('login'), $dadosTemplate)
-                ->withHeader('Content-Type', 'text/html')
-                ->withStatus(200);
-        } catch (\Exception $e) {
-            var_dump($e->getMessage());
-            die;
-        }
-    }
-    public function precadastro($request, $response)
-    {
-        try {
-            #Captura os dados do form
-            $form = $request->getParsedBody();
-            #Capturar os dados do usuário.
-            $dadosUsuario = [
-                'nome' => $form['nome'],
-                'sobrenome' => $form['sobrenome'],
-                'cpf' => $form['cpf'],
-                'rg' => $form['rg'],
-                'senha' => password_hash($form['senhaCadastro'], PASSWORD_DEFAULT)
-            ];
-            $IsInseted = InsertQuery::table('usuario')->save($dadosUsuario);
-            if (!$IsInseted) {
-                return $this->SendJson(
-                    $response,
-                    ['status' => false, 'msg' => 'Restrição: ' . $IsInseted, 'id' => 0],
-                    403
-                );
-            }
-            #Captura o código do ultimo usuário cadastrado na tabela de usuário
-            $id = SelectQuery::select('id')->from('usuario')->order('id', 'desc')->fetch();
-            #Colocamos o ID do ultimo usuário cadastrado na varaivel $id_usuario.
-            $id_usuario = $id['id'];
-            #Inserimos o e-mail
-            $dadosContato = [
-                'id_usuario' => $id_usuario,
-                'tipo' => 'email',
-                'contato' => $form['email']
-            ];
-            InsertQuery::table('contato')->save($dadosContato);
-            $dadosContato = [];
-            #Inserimos o celular
-            $dadosContato = [
-                'id_usuario' => $id_usuario,
-                'tipo' => 'celular',
-                'contato' => $form['celular']
-            ];
-            InsertQuery::table('contato')->save($dadosContato);
-            $dadosContato = [];
-            #Inserimos o WhastaApp
-            $dadosContato = [
-                'id_usuario' => $id_usuario,
-                'tipo' => 'whatsapp',
-                'contato' => $form['whatsapp']
-            ];
-            InsertQuery::table('contato')->save($dadosContato);
-            return $this->SendJson($response, ['status' => true, 'msg' => 'Cadastro realizado com sucesso!', 'id' => $id_usuario], 201);
-        } catch (\Exception $e) {
-            return $this->SendJson($response, ['status' => true, 'msg' => 'Restrição: ' . $e->getMessage(), 'id' => 0], 500);
-        }
-    }
-    public function autenticar($request, $response)
-    {
-        try {
-            #Captura os dados do form
-            $form = $request->getParsedBody();
-            #Caso a posição login não exista, informa a ocorrencia de erro.
-            if (!isset($form['login']) || empty($form['login'])) {
-                return $this->SendJson($response, ['status' => false, 'msg' => 'Por favor informe o login', 'id' => 0], 403);
-            }
-            #Caso a posição login não exista, informa a ocorrencia de erro.
-            if (!isset($form['senha']) || empty($form['senha'])) {
-                return $this->SendJson($response, ['status' => false, 'msg' => 'Por favor informe o senha', 'id' => 0], 403);
-            }
-            $user = SelectQuery::select()
-                ->from('vw_usuario_contatos')
-                ->where('cpf', '=', $form['login'], 'or')
-                ->where('email', '=', $form['login'], 'or')
-                ->where('celular', '=', $form['login'], 'or')
-                ->where('whatsapp', '=', $form['login'])
-                ->fetch();
-            if (!isset($user) || empty($user) || count($user) <= 0) {
-                return $this->SendJson(
-                    $response,
-                    ['status' => false, 'msg' => 'Usuário ou senha inválidos!', 'id' => 0],
-                    403
-                );
-            }
-            if (!$user['ativo']) {
-                return $this->SendJson(
-                    $response,
-                    ['status' => false, 'msg' => 'Por enquanto você ainda não tem permissão de acessar o sistema!', 'id' => 0],
-                    403
-                );
-            }
-            if (!password_verify($form['senha'], $user['senha'])) {
-                return $this->SendJson(
-                    $response,
-                    ['status' => false, 'msg' => 'Usuário ou senha inválidos!', 'id' => 0],
-                    403
-                );
-            }
-
-            if (password_needs_rehash($user['senha'], PASSWORD_DEFAULT)) {
-                UpdateQuery::table('usuario')->set(['senha' => password_hash($form['senha'], PASSWORD_DEFAULT)])->where('id', '=', $user['id'])->update();
-            }
-
-            $_SESSION['usuario'] = [
-                'id' => $user['id'],
-                'nome' => $user['nome'],
-                'sobrenome' => $user['sobrenome'],
-                'cpf' => $user['cpf'],
-                'rg' => $user['rg'],
-                'ativo' => $user['ativo'],
-                'logado' => true,
-                'administrador' => $user['administrador'],
-                'celular' => $user['celular'],
-                'email' => $user['email'],
-                'whatsapp' => $user['whatsapp'],
-                'data_cadastro' => $user['data_cadastro'],
-                'data_alteracao' => $user['data_alteracao'],
-            ];
-
-            return $this->SendJson(
-                $response,
-                ['status' => true, 'msg' => 'Seja bem-vindo de volta!', 'id' => $user['id']],
-                200
-            );
-        } catch (\Exception $e) {
-            return $this->SendJson($response, ['status' => false, 'msg' => 'Restrição: ' . $e->getMessage(), 'id' => 0], 500);
-        }
+        $dadosTemplate = [
+            'titulo' => 'Login'
+        ];
+        return $this->getTwig()
+            ->render($response, $this->setView('login'), $dadosTemplate)
+            ->withHeader('Content-Type', 'text/html')
+            ->withStatus(200);
     }
 }
--- app/route/route.php
+++ app/route/route.php
@@ -1,30 +1,13 @@
 <?php
 
-use app\controller\User;
 use app\controller\Home;
-use app\controller\Customer;
 use app\controller\Login;
-use app\middleware\Middleware;
 use Slim\Routing\RouteCollectorProxy;
 
-$app->get('/', Home::class . ':home')->add(Middleware::authentication());
-$app->get('/home', Home::class . ':home')->add(Middleware::authentication());
+$app->get('/', Home::class . ':home');
+$app->get('/home', Home::class . ':home');
 $app->get('/login', Login::class . ':login');
 
-$app->group('/login', function (RouteCollectorProxy $group) {
-    $group->post('/precadastro', Login::class . ':precadastro');
-    $group->post('/autenticar', Login::class . ':autenticar');
-});
-$app->group('/usuario', function (RouteCollectorProxy $group) {
-    $group->get('/lista', User::class . ':lista')->add(Middleware::authentication());
-    $group->get('/cadastro', User::class . ':cadastro')->add(Middleware::authentication());
-    $group->get('/alterar/{id}', User::class . ':alterar');
-    $group->post('/listuser', User::class . ':listuser');
-    $group->post('/insert', User::class . ':insert');
-    $group->post('/update', User::class . ':update');
-});
-$app->group('/cliente', function (RouteCollectorProxy $group) {
-    $group->get('/lista', Customer::class . ':lista');
-    $group->get('/cadastro', Customer::class . ':cadastro');
-    $group->post('/insert', Customer::class . ':insert');
+$app->group('/home', function (RouteCollectorProxy $group) {
+    #$group->post('/tema', Home::class . ':tema');
 });
