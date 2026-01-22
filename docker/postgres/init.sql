83c5fb41ad64340637f096edbff6d7dfff3ccb3b
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2026-01-22 16:00:59 -0400

    Commit

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -1,1 +1,7 @@
+-- É uma extensão oficial que fornece funções para gerar UUIDs, como uuid_generate_v4()
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- Exemplo de uso: SELECT uuid_generate_v4();
+-- Retorno: 550edb11-5ff5-489a-a26f-cec315fd4d5b
+
+
