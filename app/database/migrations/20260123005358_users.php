1aff8726d28766eea4e0d9120f4fb09c9b5a4734
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2026-01-23 17:28:13 -0400

    Commit

--- app/database/migrations/20260123005358_users.php
+++ app/database/migrations/20260123005358_users.php
@@ -8,6 +8,12 @@ final class Users extends AbstractMigration
 {
     public function change(): void
     {
-        
+        $table = $this->table('users', ['id' => false, 'primary_key' => ['id']]);
+        $table->addColumn('id', 'biginteger', ['identity' => true, 'null' => false])
+            ->addColumn('nome', 'text', ['null' => true])
+            ->addColumn('salario', 'decimal', ['null' => true, 'default' => 0, 'precision' => 18, 'scale' => 4])
+            ->addColumn('data_cadastro', 'datetime', ['null' => true, 'default' => 'CURRENT_TIMESTAMP'])
+            ->addColumn('data_atualizacao', 'datetime', ['null' => true, 'default' => 'CURRENT_TIMESTAMP'])
+            ->create();
     }
 }
