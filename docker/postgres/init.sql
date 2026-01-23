36445881133ffd7b432a56c707bfefff366dea06
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2026-01-22 20:11:14 -0400

    Commit

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -4,4 +4,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 -- Exemplo de uso: SELECT uuid_generate_v4();
 -- Retorno: 550edb11-5ff5-489a-a26f-cec315fd4d5b
 
+CREATE ROLE senac LOGIN PASSWORD 'senac';
+
+CREATE DATABASE development_db OWNER senac;
 
