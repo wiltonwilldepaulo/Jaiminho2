7b219d03da66d1d7cd1561fa6cdc77d69e7d4aeb
WILTON WILL DE PAULO <57237004+wiltonwilldepaulo@users.noreply.github.com>
2026-01-22 21:28:06 -0400

    Commit

---
+++ app/database/migrations/20260123005358_users.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class Users extends AbstractMigration
+{
+    public function change(): void
+    {
+        
+    }
+}
